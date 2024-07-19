47f98e7168e5a9040cae6c4cc192323c0734711e
lika-ruby <mary.havrylova.2011@gmail.com>
2024-07-19 23:44:06 +0300

    all done

---
+++ index.js
@@ -0,0 +1,36 @@
+const age = 12;
+console.log(age);
+
+const name = "Маріша";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Я не маю улюбленої цитати";
+console.log(myString);
+
+let myNumber = 7;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const ghostName = prompt("Як вас звати?");
+console.log(ghostName);
+
+const isConfirm = confirm("Підтвердіть дію");
+if (isConfirm) {
+  console.log("Дякую за підтвердження!");
+} else {
+  console.log("Дію відмінено!");
+}
+
+const message = alert("Підтвердіть перевод 1 милиона гривень");
+const isTransfering = confirm("Чи впевнені у цій дії");
+if (isConfirm) {
+  console.log("Дякую за підтвердження!");
+} else {
+  console.log("Дію відмінено!");
+}
